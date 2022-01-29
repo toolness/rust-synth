752cd6def9c14493cfb24875143e766cdaa6a046
Atul Varma <varmaa@gmail.com>
2022-01-29 17:57:45 -0500

    Factor out MidiNote, Semitones.

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,163 @@ use std::time::Duration;
 
 const TWO_PI: f64 = 2.0 * std::f64::consts::PI;
 
+// https://www.inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies
+const A4_MIDI_NOTE: i8 = 69;
+
+// https://en.wikipedia.org/wiki/Equal_temperament
+const A4_FREQUENCY: f64 = 440.0;
+const SEMITONES_PER_OCTAVE: i8 = 12;
+
+const SEMITONE: Semitones = Semitones(1);
+const TONE: Semitones = Semitones(2);
+const MAJOR_SCALE: [Semitones; 7] = [TONE, TONE, SEMITONE, TONE, TONE, TONE, SEMITONE];
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+struct MidiNote(i8);
+
+impl MidiNote {
+    fn frequency(&self) -> f64 {
+        // Keep in mind that every MIDI note represents a semitone.
+        let semitones_from_a4: f64 = self.0 as f64 - A4_MIDI_NOTE as f64;
+        return A4_FREQUENCY * 2.0f64.powf(semitones_from_a4 as f64 / SEMITONES_PER_OCTAVE as f64);
+    }
+
+    fn try_from_chars(
+        note: char,
+        accidental: Option<char>,
+        octave: char,
+    ) -> Result<MidiNote, MidiNoteParseError> {
+        let note_semitones_from_a = match note {
+            'C' => Semitones(-9),
+            'D' => Semitones(-7),
+            'E' => Semitones(-5),
+            'F' => Semitones(-4),
+            'G' => Semitones(-2),
+            'A' => Semitones(0),
+            'B' => Semitones(2),
+            _ => return Err(MidiNoteParseError::InvalidNoteCharacter),
+        };
+        let accidental_semitone_offset = match accidental {
+            Some('#') => Semitones(1),
+            Some('b') => Semitones(-1),
+            None => Semitones(0),
+            _ => return Err(MidiNoteParseError::InvalidAccidentalCharacter),
+        };
+        let octaves_from_4 = match octave {
+            '0' => -4,
+            '1' => -3,
+            '2' => -2,
+            '3' => -1,
+            '4' => 0,
+            '5' => 1,
+            '6' => 2,
+            '7' => 3,
+            '8' => 4,
+            '9' => 5,
+            _ => return Err(MidiNoteParseError::InvalidOctaveCharacter),
+        };
+
+        Ok(MidiNote(A4_MIDI_NOTE)
+            + note_semitones_from_a
+            + accidental_semitone_offset
+            + Semitones(octaves_from_4 * SEMITONES_PER_OCTAVE))
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum MidiNoteParseError {
+    InvalidLength,
+    InvalidNoteCharacter,
+    InvalidAccidentalCharacter,
+    InvalidOctaveCharacter,
+}
+
+impl TryFrom<&str> for MidiNote {
+    type Error = MidiNoteParseError;
+
+    fn try_from(value: &str) -> Result<MidiNote, MidiNoteParseError> {
+        match &value.chars().collect::<Vec<char>>()[..] {
+            &[note, accidental, octave] => MidiNote::try_from_chars(note, Some(accidental), octave),
+            &[note, octave] => MidiNote::try_from_chars(note, None, octave),
+            _ => Err(MidiNoteParseError::InvalidLength),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::MidiNote;
+
+    #[test]
+    fn test_a4_works() {
+        assert_eq!("A4".try_into(), Ok(MidiNote(69)));
+    }
+
+    #[test]
+    fn test_c4_works() {
+        assert_eq!("C4".try_into(), Ok(MidiNote(60)));
+    }
+
+    #[test]
+    fn test_weird_notes_work() {
+        let f4: MidiNote = "F4".try_into().unwrap();
+        assert_eq!("E#4".try_into(), Ok(f4));
+
+        let b3: MidiNote = "B3".try_into().unwrap();
+        assert_eq!("Cb4".try_into(), Ok(b3));
+    }
+
+    #[test]
+    fn test_sharps_work() {
+        assert_eq!("C#4".try_into(), Ok(MidiNote(61)));
+    }
+
+    #[test]
+    fn test_flats_work() {
+        assert_eq!("Bb4".try_into(), Ok(MidiNote(70)));
+    }
+
+    #[test]
+    fn test_octaves_work() {
+        assert_eq!("A0".try_into(), Ok(MidiNote(21)));
+        assert_eq!("G9".try_into(), Ok(MidiNote(127)));
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Semitones(i8);
+
+impl std::ops::Add<Semitones> for MidiNote {
+    type Output = MidiNote;
+
+    fn add(self, rhs: Semitones) -> MidiNote {
+        // TODO: We shouldn't let this go outside the range of MIDI notes.
+        MidiNote(self.0 + rhs.0)
+    }
+}
+
+impl std::ops::AddAssign<Semitones> for MidiNote {
+    fn add_assign(&mut self, rhs: Semitones) {
+        self.0 = self.0 + rhs.0;
+    }
+}
+
+impl std::ops::Sub<Semitones> for MidiNote {
+    type Output = MidiNote;
+
+    fn sub(self, rhs: Semitones) -> MidiNote {
+        // TODO: We shouldn't let this go outside the range of MIDI notes.
+        MidiNote(self.0 - rhs.0)
+    }
+}
+
+impl std::ops::SubAssign<Semitones> for MidiNote {
+    fn sub_assign(&mut self, rhs: Semitones) {
+        self.0 = self.0 - rhs.0;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
     let host = cpal::default_host();
     let device = host
         .default_output_device()
@@ -25,9 +180,9 @@ fn main() {
         supported_config
     );
     let config = supported_config.into();
-    let mut frequency = 261.63; // Middle C, taken from https://pages.mtu.edu/~suits/notefreqs.html
+    let tonic: MidiNote = "C4".try_into().unwrap();
     let shape_mutex = Arc::new(Mutex::new(AudioShape {
-        frequency,
+        frequency: tonic.frequency(),
         volume: 255,
     }));
     let stream = match sample_format {
@@ -38,14 +193,12 @@ fn main() {
     stream.play().unwrap();
 
     let one_beat = Duration::from_millis(500);
+    let mut note: MidiNote = tonic;
 
-    for semitones in [2, 2, 1, 2, 2, 2, 1].iter() {
+    for semitones in MAJOR_SCALE.iter() {
         thread::sleep(one_beat);
-
-        // https://en.wikipedia.org/wiki/Equal_temperament
-        frequency *= 2.0f64.powf(*semitones as f64 / 12.0);
-
-        shape_mutex.lock().unwrap().frequency = frequency;
+        note += *semitones;
+        shape_mutex.lock().unwrap().frequency = note.frequency();
     }
 
     thread::sleep(one_beat);
