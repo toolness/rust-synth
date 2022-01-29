f09cc52ac926db021f6bfda24f49d24ef6941414
Atul Varma <varmaa@gmail.com>
2022-01-29 15:47:36 -0500

    Never block audio thread.

--- src/main.rs
+++ src/main.rs
@@ -66,7 +66,8 @@ struct AudioShape {
 struct Player {
     num_channels: u16,
     sample_rate: usize,
-    shape: Arc<Mutex<AudioShape>>,
+    shape_mutex: Arc<Mutex<AudioShape>>,
+    shape: AudioShape,
     latest_pos_in_wave: f64,
     latest_volume: u8,
 }
@@ -75,11 +76,13 @@ impl Player {
     fn get_stream<T: Sample>(
         device: Device,
         config: &StreamConfig,
-        shape: Arc<Mutex<AudioShape>>,
+        shape_mutex: Arc<Mutex<AudioShape>>,
     ) -> Stream {
+        let shape = *shape_mutex.lock().unwrap();
         let mut player = Player {
             num_channels: config.channels,
             sample_rate: config.sample_rate.0 as usize,
+            shape_mutex,
             shape,
             latest_pos_in_wave: 0.0,
             latest_volume: 0,
@@ -105,9 +108,17 @@ impl Player {
         }
     }
 
+    /// Try to update the shape from our mutex, but don't block, because we're
+    /// running in an extremely time-sensitive audio thread.
+    fn try_to_update_shape(&mut self) {
+        if let Ok(shape) = self.shape_mutex.try_lock() {
+            self.shape = *shape
+        }
+    }
+
     fn write_audio<T: Sample>(&mut self, data: &mut [T], _: &cpal::OutputCallbackInfo) {
-        let shape = *self.shape.lock().unwrap();
-        let samples_per_wave = self.sample_rate as f64 / shape.frequency;
+        self.try_to_update_shape();
+        let samples_per_wave = self.sample_rate as f64 / self.shape.frequency;
         let wave_delta_per_sample = 1.0 / samples_per_wave;
 
         // We use chunks_mut() to access individual channels:
@@ -121,7 +132,7 @@ impl Player {
                 *channel_sample = sample_value;
             }
             self.latest_pos_in_wave = (self.latest_pos_in_wave + wave_delta_per_sample) % 1.0;
-            self.move_to_target_volume(shape.volume);
+            self.move_to_target_volume(self.shape.volume);
         }
     }
 }
