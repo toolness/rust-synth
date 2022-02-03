9a4ddd54467d4de074358f863ffc3a4cba26eb66
Atul Varma <varmaa@gmail.com>
2022-02-02 20:37:26 -0500

    Factor out build_stream().

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::{AppSettings, ArgEnum, Parser, Subcommand};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::SampleFormat;
+use cpal::Stream;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
@@ -8,6 +9,9 @@ use std::time::Duration;
 mod note;
 mod player;
 mod synth;
+
+use player::Player;
+use synth::AudioShape;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -34,10 +38,33 @@ enum Scale {
     MinorHarmonic,
 }
 
+fn build_stream(shape_mutex: Arc<Mutex<AudioShape>>) -> Stream {
+    let host = cpal::default_host();
+    let device = host
+        .default_output_device()
+        .expect("no output device available");
+    let mut supported_configs_range = device
+        .supported_output_configs()
+        .expect("error while querying configs");
+    let supported_config = supported_configs_range
+        .next()
+        .expect("no supported config?!")
+        .with_max_sample_rate();
+    let sample_format = supported_config.sample_format();
+    println!(
+        "Attempting to create an output stream with format: {:?}",
+        supported_config
+    );
+    let config = supported_config.into();
+    match sample_format {
+        SampleFormat::F32 => Player::get_stream::<f32>(device, &config, shape_mutex),
+        SampleFormat::I16 => Player::get_stream::<i16>(device, &config, shape_mutex),
+        SampleFormat::U16 => Player::get_stream::<u16>(device, &config, shape_mutex),
+    }
+}
+
 fn main() {
     use note::{MidiNote, Semitones, MAJOR_SCALE, MINOR_HARMONIC_SCALE};
-    use player::Player;
-    use synth::AudioShape;
 
     let cli = Args::parse();
     let tonic: MidiNote;
@@ -66,32 +93,11 @@ fn main() {
         }
     }
 
-    let host = cpal::default_host();
-    let device = host
-        .default_output_device()
-        .expect("no output device available");
-    let mut supported_configs_range = device
-        .supported_output_configs()
-        .expect("error while querying configs");
-    let supported_config = supported_configs_range
-        .next()
-        .expect("no supported config?!")
-        .with_max_sample_rate();
-    let sample_format = supported_config.sample_format();
-    println!(
-        "Attempting to create an output stream with format: {:?}",
-        supported_config
-    );
-    let config = supported_config.into();
     let shape_mutex = Arc::new(Mutex::new(AudioShape {
         frequency: tonic.frequency(),
         volume: 255,
     }));
-    let stream = match sample_format {
-        SampleFormat::F32 => Player::get_stream::<f32>(device, &config, shape_mutex.clone()),
-        SampleFormat::I16 => Player::get_stream::<i16>(device, &config, shape_mutex.clone()),
-        SampleFormat::U16 => Player::get_stream::<u16>(device, &config, shape_mutex.clone()),
-    };
+    let stream = build_stream(shape_mutex.clone());
     stream.play().unwrap();
 
     let one_beat = Duration::from_millis(500);
