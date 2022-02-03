9e39c07481f2da05b5dffa076909e55c17770c35
Atul Varma <varmaa@gmail.com>
2022-02-02 20:28:24 -0500

    Provie scale type via CLI.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{AppSettings, Parser, Subcommand};
+use clap::{AppSettings, ArgEnum, Parser, Subcommand};
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::SampleFormat;
 use std::sync::{Arc, Mutex};
@@ -21,18 +21,29 @@ struct Args {
 #[derive(Subcommand, Debug)]
 enum Commands {
     /// Plays a scale (default C4).
-    Scale { note: Option<String> },
+    Scale {
+        note: Option<String>,
+        #[clap(arg_enum)]
+        scale: Option<Scale>,
+    },
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
+enum Scale {
+    Major,
+    MinorHarmonic,
 }
 
 fn main() {
-    use note::{MidiNote, MAJOR_SCALE, MINOR_HARMONIC_SCALE};
+    use note::{MidiNote, Semitones, MAJOR_SCALE, MINOR_HARMONIC_SCALE};
     use player::Player;
     use synth::AudioShape;
 
     let cli = Args::parse();
     let tonic: MidiNote;
+    let scale_semitones: Vec<Semitones>;
     match &cli.command {
-        Commands::Scale { note } => {
+        Commands::Scale { note, scale } => {
             if let Some(note_str) = note {
                 if let Ok(note) = MidiNote::parse(note_str) {
                     tonic = note;
@@ -43,6 +54,15 @@ fn main() {
             } else {
                 tonic = "C4".try_into().unwrap()
             }
+            let base_scale = match scale.unwrap_or(Scale::Major) {
+                Scale::Major => MAJOR_SCALE,
+                Scale::MinorHarmonic => MINOR_HARMONIC_SCALE,
+            };
+            scale_semitones = base_scale
+                .iter()
+                .copied()
+                .chain(base_scale.iter().rev().map(|s| -s))
+                .collect();
         }
     }
 
@@ -77,11 +97,7 @@ fn main() {
     let one_beat = Duration::from_millis(500);
     let mut note: MidiNote = tonic;
 
-    for semitones in MAJOR_SCALE
-        .iter()
-        .copied()
-        .chain(MINOR_HARMONIC_SCALE.iter().rev().map(|s| -s))
-    {
+    for semitones in scale_semitones {
         thread::sleep(one_beat);
         note += semitones;
         shape_mutex.lock().unwrap().frequency = note.frequency();
