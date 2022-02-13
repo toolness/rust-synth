2b9e891763d3e72c42b6ab799a7aedd6fe1d24a1
Atul Varma <varmaa@gmail.com>
2022-02-13 07:45:10 -0500

    Add tests, BeatCounter::total_millis().

--- src/beat.rs
+++ src/beat.rs
@@ -68,6 +68,11 @@ impl BeatSettings {
         let ms_per_beat = beats_per_second * 1000.0;
         ms_per_beat * self.beats_in_duration(length)
     }
+
+    pub fn measure_in_millis(&self) -> f64 {
+        self.duration_in_millis(self.time_signature.beat_unit())
+            * self.time_signature.beats_per_measure() as f64
+    }
 }
 
 #[derive(Copy, Clone)]
@@ -102,5 +107,42 @@ impl BeatCounter {
 
     pub fn total_measures(&self) -> f64 {
         self.total_beats() / self.settings.time_signature.beats_per_measure() as f64
+    }
+
+    pub fn total_millis(&self) -> f64 {
+        self.total_measures() * self.settings.measure_in_millis()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::beat::Beat;
+
+    use super::{BeatCounter, BeatSettings, FOUR_FOUR};
+
+    #[test]
+    fn test_beat_settings_works() {
+        let bs = BeatSettings::new(60, FOUR_FOUR);
+        assert_eq!(bs.beats_in_duration(Beat::Quarter), 1.0);
+        assert_eq!(bs.beats_in_duration(Beat::Half), 2.0);
+        assert_eq!(bs.beats_in_duration(Beat::Eighth), 0.5);
+        assert_eq!(bs.measure_in_millis(), 4000.0);
+    }
+
+    #[test]
+    fn test_beat_counter_works() {
+        let bs = BeatSettings::new(60, FOUR_FOUR);
+        let mut bc = BeatCounter::new(bs);
+        assert_eq!(bc.total_millis(), 0.0);
+        assert_eq!(bc.increment(Beat::Quarter), 1000.0);
+        assert_eq!(bc.total_measures(), 0.25);
+        assert_eq!(bc.total_millis(), 1000.0);
+
+        bc.increment(Beat::Half);
+        bc.increment(Beat::Eighth);
+        bc.increment(Beat::Eighth);
+
+        assert_eq!(bc.total_measures(), 1.0);
+        assert_eq!(bc.total_millis(), 4000.0);
     }
 }
